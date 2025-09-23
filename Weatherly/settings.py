ba2173a0f47e72fd63455588bde5a5b891feb2e4
ankush21sali <saliankush21@gmail.com>
2025-09-23 12:48:42 +0530

    Normalize line endings

--- Weatherly/settings.py
+++ Weatherly/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^-o@w+e=t0g3gcd%03jg($p01u-ra*dv(#(4!sfpjjla(zpke&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,8 +119,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# environment variable for OpenWeather API
+OPENWEATHER_API_KEY = os.getenv('OPENWEATHER_API_KEY', 'a09cb56c1721ef2dc7e9ae11ffee0191')
